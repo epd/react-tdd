d01a7c9af84313f84fb45da54be325893138f4c6
Evan Dudla <evan@dud.la>
2019-10-20 20:57:07 +0700

    step 4: transforms list elements

--- List.js
+++ List.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+const doSomeListMagic = (item, idx) =>
+    idx % 2 === 0 ? item.toUpperCase() : item.toLowerCase();
+
 const List = ({items}) => (
     <ul>
         {items.map((item, idx) => (
-            <li key={idx}>{item}</li>
+            <li key={idx}>{doSomeListMagic(item, idx)}</li>
         ))}
+
+        {/* <li key={idx}>{item.toUpperCase()}</li> */}
     </ul>
 );
 
--- List.unit.spec.js
+++ List.unit.spec.js
@@ -16,11 +16,11 @@ describe('List', () => {
         const wrapper = shallow(<List items={items} />);
 
         expect(wrapper.find('li').length).toBe(5);
-        expect(wrapper.find('li').at(0).text()).toBe('Villena'); // prettier-ignore
-        expect(wrapper.find('li').at(1).text()).toBe('Madrid'); // prettier-ignore
-        expect(wrapper.find('li').at(2).text()).toBe('San Francisco'); // prettier-ignore
-        expect(wrapper.find('li').at(3).text()).toBe('Nashville'); // prettier-ignore
-        expect(wrapper.find('li').at(4).text()).toBe('Vancouver'); // prettier-ignore
+        expect(wrapper.find('li').at(0).text()).toBe('VILLENA'); // prettier-ignore
+        expect(wrapper.find('li').at(1).text()).toBe('madrid'); // prettier-ignore
+        expect(wrapper.find('li').at(2).text()).toBe('SAN FRANCISCO'); // prettier-ignore
+        expect(wrapper.find('li').at(3).text()).toBe('nashville'); // prettier-ignore
+        expect(wrapper.find('li').at(4).text()).toBe('VANCOUVER'); // prettier-ignore
 
         // expect(wrapper.find('ul').text()).toBe(items.join(''));
         // expect(wrapper.find('ul')).toMatchSnapshot();
